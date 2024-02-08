9fe60f0b7776eb58b2696e9f0590be9eb7e72a74
Chris Wilhelm <chris.wilhelm@gmail.com>
2024-02-08 13:14:37 -0800

    Stub (#2)

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+rm -rf dist
+
+export TWINE_NON_INTERACTIVE=1
+export TWINE_USERNAME='__token__'
+export TWINE_PASSWORD=$AI2_NORA_PYPI_TOKEN
+
+pip install --upgrade pip setuptools wheel build twine
+python -m build
+twine upload dist/*
