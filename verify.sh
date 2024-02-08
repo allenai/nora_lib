9ea08bfb7d49267ec4a970e29ecb1bbc1feedba3
Chris Wilhelm <chris.wilhelm@gmail.com>
2024-02-08 13:14:37 -0800

    Stub (#2)

---
+++ verify.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+FAILED=0
+FAILURE_REASONS=()
+
+export TAG_SUFFIX="${BUILD_NUMBER:-$(date +%s)}"
+
+add_failure () {
+  FAILED=1
+  FAILURE_REASONS+=("Failed $1. Investigate locally with: $2")
+}
+
+make build-image || add_failure "image build" "make build-image"
+make check-format || add_failure "formatting" "make format"
+make mypy || add_failure "type checking" "make mypy"
+make pytest || add_failure "unit tests" "make pytest"
+
+for failure_reason in "${FAILURE_REASONS[@]}"
+do
+  echo "$failure_reason" >&2
+done
+
+exit $FAILED
