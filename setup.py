f9954b54393cca729638a1654e619af3e87771f1
stefanc-ai2 <stefanc@allenai.org>
2024-05-21 15:33:24 -0700

    up version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dev_requirements = ["mypy", "pytest", "black", "types-requests"]
 
 setuptools.setup(
     name="nora_lib",
-    version="0.0.6.dev1",
+    version="0.0.6",
     description="For making and coordinating agents and tools",
     url="https://github.com/allenai/nora_lib",
     packages=setuptools.find_packages(exclude=(["tests"])),
