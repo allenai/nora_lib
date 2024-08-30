e34270e6060781aea573b3b47084006cd8928341
stefanc-ai2 <stefanc@allenai.org>
2024-08-30 11:29:05 -0700

    step cost with example

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dev_requirements = ["mypy", "pytest", "black", "types-requests"]
 
 setuptools.setup(
     name="nora_lib",
-    version="0.2.8",
+    version="0.3.0",
     description="For making and coordinating agents and tools",
     url="https://github.com/allenai/nora_lib",
     packages=setuptools.find_packages(exclude=(["tests"])),
