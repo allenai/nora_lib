482ebc95e0227d135a6368dba716bee7e1a72886
stefanc-ai2 <stefanc@allenai.org>
2024-10-23 16:56:45 -0700

    fix mypy, add iservice tests

--- tests/integration/interactions/test_interactions_service.py
+++ tests/integration/interactions/test_interactions_service.py
@@ -10,6 +10,29 @@ THREAD = str(uuid4())
 CHANNEL = str(uuid4())
 
 
+def _msg(text: str):
+    return Message(
+        message_id=str(uuid4()),
+        actor_id=ACTOR,
+        text=text,
+        channel_id=CHANNEL,
+        thread_id=THREAD,
+        surface=Surface.WEB,
+        ts=datetime.now(),
+    )
+
+
+def _event(msg: Message, event_type: str, data: dict):
+    return Event(
+        type=event_type,
+        actor_id=ACTOR,
+        timestamp=datetime.now(),
+        text="",
+        data=data,
+        message_id=msg.message_id,
+    )
+
+
 class TestInteractionsService(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
@@ -18,28 +41,13 @@ class TestInteractionsService(unittest.TestCase):
             token="test",
         )
 
+    def test_save_message(self):
+        message = _msg("Hi")
+        self.iservice.save_message(message)
+        returned_message = self.iservice.get_message(message.message_id)
+        self.assertEqual(returned_message.text, message.text)
+
     def test_virtual_thread(self):
-        def _msg(text: str):
-            return Message(
-                message_id=str(uuid4()),
-                actor_id=ACTOR,
-                text=text,
-                channel_id=CHANNEL,
-                thread_id=THREAD,
-                surface=Surface.WEB,
-                ts=datetime.now(),
-            )
-
-        def _event(msg: Message, type: str, data: dict):
-            return Event(
-                type=type,
-                actor_id=ACTOR,
-                timestamp=datetime.now(),
-                text="",
-                data=data,
-                message_id=msg.message_id,
-            )
-
         virtual_thread_1 = "virtual_thread_1"
         virtual_thread_2 = "virtual_thread_2"
         msg1 = _msg("Hi 1")
@@ -62,3 +70,12 @@ class TestInteractionsService(unittest.TestCase):
         self.assertEqual([m.message_id for m in content], [msg2.message_id])
         # Should only contain the events tagged with virtual_thread_1
         self.assertEqual([e.type for e in content[0].events], [event2.type])
+
+    def test_save_event(self):
+        message = _msg("Hi")
+        self.iservice.save_message(message)
+        event = _event(message, "event", {"key": "value"})
+        event_id = self.iservice.save_event(event)
+        returned_event = self.iservice.get_event(event_id)
+        self.assertEqual(returned_event.data, event.data)
+        self.assertEqual(returned_event.type, event.type)
