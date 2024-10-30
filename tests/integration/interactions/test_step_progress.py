c419a303829f890abddefabb7d4039edaeee75e6
stefanc-ai2 <stefanc@allenai.org>
2024-10-30 12:20:55 -0700

    pubsub service

--- tests/integration/interactions/test_step_progress.py
+++ tests/integration/interactions/test_step_progress.py
@@ -1,6 +1,8 @@
 import unittest
 from uuid import uuid4
 
+from nora_lib.pubsub import PubsubService
+
 from nora_lib.interactions.step_progress import (
     StepProgressReporter,
     StepProgress,
@@ -27,14 +29,18 @@ def _msg(text: str) -> Message:
     )
 
 
-def _spr(iservice: InteractionsService) -> StepProgressReporter:
+def _spr(
+    iservice: InteractionsService, pubsub_service: PubsubService
+) -> StepProgressReporter:
     message = _msg("Hi")
     iservice.save_message(message)
     return StepProgressReporter(
         actor_id=ACTOR,
         message_id=message.message_id,
+        thread_id=message.thread_id,
         step_progress=StepProgress(short_desc="short step"),
         interactions_service=iservice,
+        pubsub_service=pubsub_service,
     )
 
 
@@ -44,9 +50,10 @@ class TestStepProgressReporter(unittest.TestCase):
         cls.iservice = InteractionsService(
             "http://interaction_service:9080", token="test"
         )
+        cls.pubsub_service = PubsubService("test", "test")
 
     def test_create_start_finish_success(self):
-        spr = _spr(self.iservice)
+        spr = _spr(self.iservice, self.pubsub_service)
         self.assertEqual(spr.step_progress.run_state, RunState.CREATED)
         self.assertIsNotNone(spr.step_progress.created_at)
 
@@ -59,7 +66,7 @@ class TestStepProgressReporter(unittest.TestCase):
         self.assertIsNotNone(spr.step_progress.finished_at)
 
     def test_finish_after_finish(self):
-        spr = _spr(self.iservice)
+        spr = _spr(self.iservice, self.pubsub_service)
         spr.start()
         spr.finish(is_success=False, error_message="error")
         failed_at = spr.step_progress.finished_at
@@ -71,13 +78,13 @@ class TestStepProgressReporter(unittest.TestCase):
         self.assertEqual(spr.step_progress.finished_at, failed_at)
 
     def test_finish_before_start(self):
-        spr = _spr(self.iservice)
+        spr = _spr(self.iservice, self.pubsub_service)
         spr.finish(is_success=True)
         self.assertEqual(spr.step_progress.run_state, RunState.CREATED)
         self.assertIsNone(spr.step_progress.finished_at)
 
     def test_start_after_start(self):
-        spr = _spr(self.iservice)
+        spr = _spr(self.iservice, self.pubsub_service)
         spr.start()
         started_at = spr.step_progress.started_at
         self.assertIsNotNone(started_at)
@@ -88,7 +95,7 @@ class TestStepProgressReporter(unittest.TestCase):
         self.assertEqual(spr.step_progress.started_at, started_at)
 
     def test_start_after_finish(self):
-        spr = _spr(self.iservice)
+        spr = _spr(self.iservice, self.pubsub_service)
         spr.start()
         spr.finish(is_success=True)
         finished_at = spr.step_progress.finished_at
@@ -100,12 +107,14 @@ class TestStepProgressReporter(unittest.TestCase):
         self.assertEqual(spr.step_progress.finished_at, finished_at)
 
     def test_create_child_step(self):
-        spr = _spr(self.iservice)
+        spr = _spr(self.iservice, self.pubsub_service)
         child_spr = spr.create_child_step(short_desc="child step")
-        self.assertEqual(child_spr.step_progress.parent_step_id, spr.step_progress.step_id)
+        self.assertEqual(
+            child_spr.step_progress.parent_step_id, spr.step_progress.step_id
+        )
 
     def test_with_context_management_success(self):
-        with _spr(self.iservice) as spr:
+        with _spr(self.iservice, self.pubsub_service) as spr:
             spr.start()
 
         # Should finish on its own
@@ -113,7 +122,7 @@ class TestStepProgressReporter(unittest.TestCase):
 
     def test_with_context_management_catch_exception(self):
         error_message = "whoops"
-        with _spr(self.iservice) as spr:
+        with _spr(self.iservice, self.pubsub_service) as spr:
             spr.start()
             raise RuntimeError(error_message)
 
