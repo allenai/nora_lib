e1c2e15eff760efbc205b7aabed169df2d78d1e5
stefanc-ai2 <stefanc@allenai.org>
2024-10-31 18:54:35 -0700

    remove else

--- nora_lib/interactions/step_progress.py
+++ nora_lib/interactions/step_progress.py
@@ -145,13 +145,13 @@ class StepProgressReporter:
                 f"Run state: {self.step_progress.run_state}."
             )
             return None
-        else:
-            self.step_progress.started_at = datetime.now(timezone.utc)
-            self.step_progress.run_state = RunState.RUNNING
-            event_id_opt = self._save_progress_to_istore()
-            if event_id_opt:
-                self._publish_to_topic(event_id_opt, self.step_progress.started_at)
-            return event_id_opt
+        
+        self.step_progress.started_at = datetime.now(timezone.utc)
+        self.step_progress.run_state = RunState.RUNNING
+        event_id_opt = self._save_progress_to_istore()
+        if event_id_opt:
+            self._publish_to_topic(event_id_opt, self.step_progress.started_at)
+        return event_id_opt
 
     def finish(
         self, is_success: bool, error_message: Optional[str] = None
