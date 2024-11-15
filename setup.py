8d6afa5f95059a25f4036b8ed1dc5f7433007b5c
stefanc-ai2 <stefanc@allenai.org>
2024-11-15 13:16:28 -0800

    parent progress step id

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dev_requirements = ["mypy", "pytest", "black", "types-requests"]
 
 setuptools.setup(
     name="nora_lib",
-    version="0.5.7rc1",
+    version="0.5.7",
     description="For making and coordinating agents and tools",
     url="https://github.com/allenai/nora_lib",
     packages=setuptools.find_packages(exclude=(["tests"])),
