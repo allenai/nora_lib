ebbd6e836114384d1913bc1373a47eb570ca9c12
stefanc-ai2 <stefanc@allenai.org>
2024-11-25 14:22:20 -0800

    reduce dupe

--- nora_lib/interactions/interactions_service.py
+++ nora_lib/interactions/interactions_service.py
@@ -345,7 +345,6 @@ class InteractionsService:
                         "events": event_query,
                     }
                 },
-                "messages": message_query,
             },
         }
         response = self._post(
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dev_requirements = ["mypy", "pytest", "black", "types-requests"]
 
 setuptools.setup(
     name="nora_lib",
-    version="0.5.10",
+    version="0.5.11",
     description="For making and coordinating agents and tools",
     url="https://github.com/allenai/nora_lib",
     packages=setuptools.find_packages(exclude=(["tests"])),
