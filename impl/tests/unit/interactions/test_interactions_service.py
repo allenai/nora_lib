717c7e6869975b677b69aab6156742b4c78cc4b8
Chloe Alcestes Anastasiades <chloea@allenai.org>
2025-11-04 09:59:36 -0800

    first case

--- impl/tests/unit/interactions/test_interactions_service.py
+++ impl/tests/unit/interactions/test_interactions_service.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import datetime
 from uuid import uuid4
-from requests import Response
+from requests import HTTPError, Response
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
@@ -10,11 +10,10 @@ from nora_lib.impl.interactions.models import Event
 
 
 class TestInteractionsService(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(0, 0)
-
     @patch("requests.request")
-    def test_something2(self, req_mock):
+    def test_retries_default(self, req_mock):
+        # Don't change anything about the retry config
+        iservice = InteractionsService("somwhere")
 
         event_in = Event(
             type="step_progress",
@@ -24,23 +23,21 @@ class TestInteractionsService(unittest.TestCase):
         )
 
         def mk_response(status_code):
-            # resp = MagicMock()
-            # resp.status_code = status_code
-            # return resp
             resp = Response()
             resp.status_code = status_code
             resp.json = MagicMock(return_value = {"events": [event_in]})
             return resp
 
-        # to_return_options = 
-        # to_return_idx = 0
-        # def to_return():
-        #     pass
-        # req_mock.return_value = "bye"
-
-        # req_mock.side_effect = [mk_response(200)]
-        req_mock.side_effect = [mk_response(500), mk_response(200)]
-        iservice = InteractionsService("somwhere")
+        # Since we haven't changed the retry config, we only have one try.
+        # Say it's successful.
+        req_mock.side_effect = [mk_response(200)]
         event_out = iservice.get_event("hi")
-
         self.assertEqual(event_in, event_out)
+
+        # Suppose the first try is not successful
+        for error_status in [500, 501, 400]:
+            req_mock.side_effect = [mk_response(error_status)]
+            with self.assertRaises(HTTPError) as exc:
+                event_out = iservice.get_event("hi2")
+            # Make sure the right status code is getting plumbed through
+            self.assertIn(str(error_status), str(exc.exception))
